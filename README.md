b1fdd74849cd5ca32e44776d91950661b7020370
AlinesantosCS <alinesantos.acs97@gmail.com>
2021-05-27 18:19:59 -0300

    Adicionado conteúdos

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
-# Steam
-Projeto do VamosAi que tem como objetivo a análise dos dados da Steam
+[![markdown][image-sla]][image-url]     
+ 
+[image-sla]: img/capa_steam.gif  
+[image-url]: https://store.steampowered.com/about/ 
+***
+# Projeto Vamos Ai
+## Módulo 3
+
+### Colaboradores do projeto 
+
+**Thiago Silva**
+
+[![MARKDOWN](img/GitHub.png)](https://github.com/tsffarias)
+
+**Aline Santos**
+
+[![MARKDOWN](img/GitHub.png)](https://github.com/AlinesantosCS)
+
+**Gabriel Borges**
+
+[![MARKDOWN](img/GitHub.png)](https://github.com/gabriellborges)
+
+**Inajá Morais**
+
+[![MARKDOWN](img/GitHub.png)](https://github.com/InaMorais)
+
+***
+## O que é a Steam ?
+> É uma plataforma de loja online de jogos de computador que foi criada pela produtora Valve em 2003.
+***
+## Ferramentas e conceitos utilizados 
+* Design thinking
+* Scrum
+* Kanban
+* Brainstorm
+* Trello
+* Metabase
+* Python(Biblioteca Pandas)
+* Storytelling
+***
+## Nosso Dashboard
+> Foram criadas perguntas para analisar os dados da Steam e para a visualização dos dados foi usado a ferramenta metabase, anteriormente realizamos a seleção das perguntas em SQL para depois utilizar o metabase.
+
+### Perguntas 
+1. Principais publicadoras (publishers)
+2. Top 10 preços mais frequentes no steam
+3. Quais categorias dos jogos mais caros;
+4. Quais sistemas operacionais mais usados;
+5. Top 10 gêneros maiores generos;
+6. Top 10 maiores categorias de jogos (categories);
+7. Idade permitida de cada jogo
+8. Quantos jogos tem o idioma inglês, e quantos não tem;
+9. Top 10 jogos com maior avaliação positiva (positive_ratings)
+10. Top 10 jogos com maior avaliação negativa (negativa_ratings)
+11. Quais jogos mais jogados (average_playtime)
+12. Quais anos tem maior numero de lançamento de  jogos (release_date)
+13. Principais developers;
+14. Quantidade de jogos por mês de lançamento
+15. Top 10 maiores categorias de jogos
+16. Top 10 produtos mais caros
+
+***
+## Visualização dos dados 
+***
+![MARKDOWN](img/parte1.png)
+
+![MARKDOWN](img/parte2.png)
+
+![MARKDOWN](img/parte3.png)
+
+![MARKDOWN](img/parte4.png)
+
+![MARKDOWN](img/parte5.png)
+
+![MARKDOWN](img/parte6.png)
+
+***
+### Informações sobre o trabalho ###
+ **Arquivos com dados da tabela:**
+* steam_description_data.sql
+* steam_requirements_data.sql
+* steam.sql
+* steamspy_tag_data.sql
+
+**Arquivo com as criações das tabelas e os comandos de consulta das perguntas feitas no trabalho:**
+* steam_principal.sql
+***
--- steam_principal.sql
+++ steam_principal.sql
@@ -1,3 +1,5 @@
+-- CRIAÇÃO DAS TABELAS
+
 CREATE TABLE steam_description_data (
 	steam_appid SERIAL PRIMARY KEY,
 	detailed_description TEXT NOT NULL,
@@ -50,8 +52,7 @@ create table steamspy_tag_data (
 	e_sports INT
 );
 
---docker cp C:\Users\Thiago\Documents\steam_description_data.csv postgres:/home
--- Comando Copy
+
 COPY steam_description_data (steam_appid, detailed_description, about_the_game, short_description)
 FROM '/home/steam_description_data.csv'
 DELIMITER ','
@@ -59,7 +60,7 @@ CSV HEADER
 
 SELECT * FROM steam_description_data
 
---docker cp C:\Users\Thiago\Documents\steam.csv postgres:/home
+
 COPY steam (appid,
 	name,
 	release_date,
@@ -84,8 +85,7 @@ CSV HEADER
 
 SELECT * FROM steam
 
---docker cp C:\Users\Thiago\Documents\steam_requirements_data.csv postgres:/home
--- Comando Copy
+
 COPY steam_requirements_data (steam_appid,
 	pc_requirements,
 	mac_requirements,
@@ -99,8 +99,7 @@ CSV HEADER
 SELECT * FROM steam_requirements_data
 
 
---docker cp C:\Users\Thiago\Documents\steamspy_tag_data.csv postgres:/home
--- Comando Copy
+
 COPY steamspy_tag_data (appid,
 	action,
 	adventure,
@@ -119,5 +118,217 @@ CSV HEADER
 
 select * from steamspy_tag_data;
 
+-- CONSULTAS
 
+-- 1. Principais publicadoras (publishers)
+select "game publishers", count(appid) as "Quantidade de jogos"
+from (
+select appid, unnest(string_to_array("publisher", ';')) as "game publishers"
+from steam
+) as game_publishers
+group by "game publishers"
+order by count(appid) desc
+limit 10
+
+-- 2. Top 10 preços mais frequentes no steam
+select price, count() as "quantidade"
+from steam
+group by price
+order by count() desc
+limit 10;
+
+-- 3. Quais categorias dos jogos mais caros;
+select "Categorias", sum(price)
+from (
+select appid, categories, unnest(string_to_array("categories", ';')) as "Categorias", price
+from steam
+) as "categorias_steam"
+group by "Categorias"
+order by sum(price) desc
+limit 10;
+
+-- 4. Quais sistemas operacionais mais usados;
+select "Sistema Operacional", count(*) as "Quantidade de Jogos"
+from (
+select platforms, unnest(string_to_array("platforms", ';')) as "Sistema Operacional"
+from steam
+) as so
+group by "Sistema Operacional";
+
+-- 5. Top 10 gêneros maiores generos;
+-- TOP 10 jogos ação
+select steamspy_tag_data.appid, steam.name, steamspy_tag_data.action
+from steamspy_tag_data
+join steam on steam.appid = steamspy_tag_data.appid
+where steamspy_tag_data.action <> 0
+order by steamspy_tag_data.action desc
+limit 10;
+
+-- Quantidade de jogos ação
+select count(*) as "Quantidade de Jogos Ação"
+from steamspy_tag_data
+join steam on steam.appid = steamspy_tag_data.appid
+where steamspy_tag_data.action <> 0
+
+-- TOP 10 jogos e-sports
+select steamspy_tag_data.appid, steam.name, steamspy_tag_data.e_sports
+from steamspy_tag_data
+join steam on steam.appid = steamspy_tag_data.appid
+where steamspy_tag_data.e_sports <> 0
+order by steamspy_tag_data.e_sports desc
+limit 10
+
+-- Quantidade de jogos e-sports
+select count(*) as "Quantidade de Jogos E_Sports"
+from steamspy_tag_data
+join steam on steam.appid = steamspy_tag_data.appid
+where steamspy_tag_data.e_sports <> 0
+
+-- TOP 10 jogos moba
+select steamspy_tag_data.appid, steam.name, steamspy_tag_data.moba
+from steamspy_tag_data
+join steam on steam.appid = steamspy_tag_data.appid
+where steamspy_tag_data.moba <> 0
+order by steamspy_tag_data.moba desc
+limit 10
+
+-- Quantidade de jogos moba
+select count(*) as "Quantidade de Jogos de moba"
+from steamspy_tag_data
+join steam on steam.appid = steamspy_tag_data.appid
+where steamspy_tag_data.moba <> 0
+
+-- TOP 10 jogos indie
+select steamspy_tag_data.appid, steam.name, steamspy_tag_data.indie
+from steamspy_tag_data
+join steam on steam.appid = steamspy_tag_data.appid
+where steamspy_tag_data.indie <> 0
+order by steamspy_tag_data.indie desc
+limit 10
+
+-- Quantidade de jogos indie
+select count(*) as "Quantidade de Jogos de indie"
+from steamspy_tag_data
+join steam on steam.appid = steamspy_tag_data.appid
+where steamspy_tag_data.indie <> 0
+
+-- TOP 10 jogos fps
+select steamspy_tag_data.appid, steam.name, steamspy_tag_data.fps
+from steamspy_tag_data
+join steam on steam.appid = steamspy_tag_data.appid
+where steamspy_tag_data.fps <> 0
+order by steamspy_tag_data.fps desc
+limit 10
+
+-- Quantidade de jogos fps
+select count(*) as "Quantidade de Jogos de FPS"
+from steamspy_tag_data
+join steam on steam.appid = steamspy_tag_data.appid
+where steamspy_tag_data.fps <> 0
+
+select steamspy_tag_data.appid, steam.name, steamspy_tag_data.adventure
+from steamspy_tag_data
+join steam on steam.appid = steamspy_tag_data.appid
+where steamspy_tag_data.adventure <> 0
+order by steamspy_tag_data.adventure desc
+limit 10
+
+-- Quantidade de jogos Aventura
+select count(*) as "Quantidade de Jogos de Aventura"
+from steamspy_tag_data
+join steam on steam.appid = steamspy_tag_data.appid
+where steamspy_tag_data.adventure <> 0
+
+
+-- 6. Top 10 maiores categorias de jogos (categories);
+select "Categorias", count(appid) as "Quantidade de Produtos"
+from (
+select appid, categories, unnest(string_to_array("categories", ';')) as "Categorias"
+from steam
+) as "categorias_steam"
+group by "Categorias"
+order by count(appid) desc
+limit 10
+
+-- 7. Idade permitida de cada jogo
+select required_age as "Idade requerida", count(appid) as "Quantidade de jogos"
+from steam
+group by required_age
+order by count(appid) desc
+
+-- 8. Quantos jogos tem o idioma inglês, e quantos não tem;
+SQL:
+SELECT
+CASE
+WHEN english = 1 THEN 'Idioma Inglês'
+ELSE 'Outro Idioma'
+END AS idioma,
+COUNT(appid) AS "Quantidade de jogos" FROM steam
+GROUP BY idioma;
+
+-- 9. Top 10 jogos com maior avaliação positiva (positive_ratings)
+select appid, name, positive_ratings
+from steam
+order by positive_ratings desc
+limit 10;
+
+-- 10. Top 10 jogos com maior avaliação negativa (negativa_ratings)
+select appid, name, negative_ratings
+from steam
+order by negative_ratings desc
+limit 10;
+
+-- 11. Quais jogos mais jogados (average_playtime)
+select appid, name, average_playtime from steam
+order by average_playtime desc
+limit 10;
+
+-- 12. Quais anos tem maior numero de lançamento de  jogos (release_date)
+-- Quais anos tem maior numero de lançamento de jogos
+select "year_release_date" as "Ano de lançamento", count(*) as "Quantidade de Jogos"
+from
+(
+select appid, extract(year from release_date)::varchar as "year_release_date"
+from steam
+) as "games_year_release_date"
+group by "year_release_date"
+order by "Quantidade de Jogos" desc
+limit 10;
+
+-- 13. Principais developers;
+select "Desenvolvedoras de jogos", count(appid) as "Quantidade de jogos"
+from (
+select appid, unnest(string_to_array("developer", ';')) as "Desenvolvedoras de jogos"
+from steam
+) as game_publishers
+group by "Desenvolvedoras de jogos"
+order by count(appid) desc
+limit 10;
+
+-- 14. Quantidade de jogos por mês de lançamento
+select
+TO_CHAR(TO_DATE ("month_release_date"::text, 'MM'), 'Month') as "Mês de lançamento",
+count(*) as "Quantidade de Jogos"
+from
+(
+select appid, release_date, extract(month from release_date) as "month_release_date"
+from steam
+) as "games_month_release_date"
+group by "month_release_date"
+order by "Quantidade de Jogos" desc
+
+-- 15. Top 10 maiores categorias de jogos
+select "Categorias", count(appid) as "Quantidade de Produtos"
+from (
+select appid, categories, unnest(string_to_array("categories", ';')) as "Categorias"
+from steam
+) as "categorias_steam"
+group by "Categorias"
+order by count(appid) desc
+limit 10
+
+-- 16. Top 10 produtos mais caros
+Comando pra obter o resultado.
+select name, price from steam
+order by price desc limit 10
 
